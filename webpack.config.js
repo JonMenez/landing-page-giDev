a4ade0a6667fe91c563fb566e84a5c788b78fe57
Joan Gimenez <joanf.gimenez@gmail.com>
2022-01-06 20:32:23 -0300

    [ADD] alias @containers, @styles

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,8 @@ module.exports = {
 		extensions: ['.js', '.jsx'],
 		alias: {
 			'@components': path.resolve(__dirname, 'src/components/'),
+			'@containers': path.resolve(__dirname, 'src/containers/'),
+			'@styles': path.resolve(__dirname, 'src/styles/'),
 			
 		}
 	},
