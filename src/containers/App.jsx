b0b852bb3ae63168aad58c1b8b8aafd05f4f6fcb
Joan Gimenez <joanf.gimenez@gmail.com>
2022-01-06 20:35:20 -0300

    [ADD] Header container

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import * as React from 'react'
+import Header from '@containers/Header'
+import '@styles/app.scss'
 
 const App = () => {
     return (
         <div>
-            <h1>Hello</h1>
+            <Header />
         </div>
     )
 }
