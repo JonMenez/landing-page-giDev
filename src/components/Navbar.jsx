d3c01f4976b996999c8d1ab0040a84e394f68ac1
Joan Gimenez <joanf.gimenez@gmail.com>
2022-01-10 15:21:16 -0300

    [UPDATE] list of links & active state button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import * as React  from 'react';
+import navButton   from '@icons/nav-button.svg'
+import closeButton from '@icons/close-button.svg'
+import '@styles/navBar.scss'
 
 const Navbar = () => {
+
+    const [active, setActive] = React.useState(false)
+
+    const handleClick = () => {
+        setActive(!active)
+    }
+
     return (
         <nav className='navBar'>
-          <ul className='navBar__list'>
-              <li className='navBar__items'><a className='navBar__links' href="">Home</a></li>
-              <li className='navBar__items'><a className='navBar__links' href="">Services</a></li>
-              <li className='navBar__items'><a className='navBar__links' href="">Projects</a></li>
-              <li className='navBar__items'><a className='navBar__links' href="">Contact</a></li>
+        <button onClick={handleClick} className='navBar__button'><img className='navBar__button-img' src={ active ? closeButton : navButton } alt='navigation button' /></button>
+          <ul className='navBar__list' id={active ? 'navBar__hidden' : '' }>
+              <li className='navBar__items'><a className='navBar__links' href="#">Home</a></li>
+              <li className='navBar__items'><a className='navBar__links' href="#">Services</a></li>
+              <li className='navBar__items'><a className='navBar__links' href="#">Projects</a></li>
+              <li className='navBar__items'><a className='navBar__links' href="#">Contact</a></li>
           </ul> 
         </nav>
     )
