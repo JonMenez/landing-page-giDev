88b00d1e70aa6c33e878121d80800e7bd32db0ba
Joan Gimenez <joanf.gimenez@gmail.com>
2022-01-17 16:26:44 -0300

    [ADD] Swiper syntax

--- src/containers/Feedback.jsx
+++ src/containers/Feedback.jsx
@@ -1,12 +1,28 @@
 import * as React from 'react'
 import '@styles/feedback.scss'
 import FeedCard from '../components/FeedCard'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation} from 'swiper';
+import 'swiper/scss';
+import 'swiper/scss/pagination';
+import 'swiper/scss/navigation';
 
 const Feedback = () => {
     return (
         <section className='feedback'>
            <h2 className='feedback__title'>What my clients are saying about me</h2>
-           <FeedCard/>
+        <Swiper
+         modules={[ Pagination, Navigation]}
+         spaceBetween={50}
+         slidesPerView={1}
+         tag="div"
+         navigation
+         pagination={{ clickable: true }} 
+           >
+            <SwiperSlide><FeedCard/></SwiperSlide>
+            <SwiperSlide><FeedCard/></SwiperSlide>
+        </Swiper>
+           
         </section>
     )
 }
