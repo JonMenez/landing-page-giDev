1ade027081352b210b9487c7b14d179d37f6c69f
Joan Gimenez <joanf.gimenez@gmail.com>
2022-01-20 11:50:55 -0300

    [UPDATE] logic button navBar moved to Header

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,13 +1,23 @@
 import * as React from 'react'
 import Navbar     from '@components/Navbar'
 import Logo       from '@components/Logo'
+import navButton   from '@icons/nav-button.svg'
+import closeButton from '@icons/close-button.svg'
 import '@styles/header.scss'
 
 const Header = () => {
+
+    const [active, setActive] = React.useState(false)
+
+    const handleClick = () => {
+        setActive(!active)
+    }
+
     return (
         <header className='header'>
             <Logo />
-            <Navbar />
+            <Navbar active={active ? 'navBar__hidden' : '' }/>
+            <button onClick={handleClick} className='navBar__button'><img className='navBar__button-img' src={ active ? closeButton : navButton } alt='navigation button' /></button>
         </header>
     )
 }
