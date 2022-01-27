c02651022688afd7478ac61f14d08d5e98f76fa3
Joan Gimenez <joanf.gimenez@gmail.com>
2022-01-27 10:52:08 -0300

    [ADD] Slider component

--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -1,19 +1,16 @@
 import * as React from 'react'
-import CardProject from '@components/CardProject'
 import wave1 from '@icons/p1-wave.svg'
 import wave2 from '@icons/p2-wave.svg'
+import Slider from '../components/Slider'
 import '@styles/projects.scss'
 
 const Projects = () => {
     return (
         <section className='projects'>
             <img className='projects__wave1' src={ wave1 } alt="svg wave" />
-            <h2 className='projects__title'>My lastest Projects</h2>
-            <div className='projects__slider'>
-              <CardProject />
-              <CardProject />
-            </div>
-            <a href='#' className="projects__more">See all</a>
+              <h2 className='projects__title'>My lastest Projects</h2>
+              <Slider idStyle='projects__slider'/>
+              <a href='#' className="projects__more">See all</a>
             <img className='projects__wave2' src={ wave2 } alt="svg wave" />
         </section>
     )
