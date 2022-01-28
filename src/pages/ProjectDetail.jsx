ae92b4eb9a52a73b823231957d1e52c3537bc2af
Joan Gimenez <joanf.gimenez@gmail.com>
2022-01-27 23:47:03 -0300

    [ADD] projects page

---
+++ src/pages/ProjectDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectDetail = () => {
+  return (
+      <div>
+          Projec Detail
+      </div>
+  )
+};
+
+export default ProjectDetail;
---
+++ src/pages/ProjectsPage.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Slider from '@components/Slider'
+
+const ProjectsPage = () => {
+  return (
+      <section>
+          <h1>List of projects</h1>
+      </section>
+  );
+};
+
+export default ProjectsPage;
