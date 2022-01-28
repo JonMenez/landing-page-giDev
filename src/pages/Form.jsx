92b82b5e043a5842feece001d7d9a94cd153ef30
Joan Gimenez <joanf.gimenez@gmail.com>
2022-01-28 16:09:15 -0300

    [UPDATE] adding FormContainer

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,18 +1,13 @@
 import * as React from 'react';
+import FormContainer from '@containers/FormContainer';
 import '@styles/form.scss'
 
 const Form = () => {
   return (
       <section className='form'>
           <h1 className="form__title">Contact</h1>
-          <form action="" className="form__container">
-              <input type="text" className="form__input" />
-              <label htmlFor="#" className="form__label">Full Name</label>
-              <input type="text" className="form__input" />
-              <label htmlFor="#" className="form__label">eMail</label>
-              <textarea name="" id="" className="form__textarea"></textarea>
-              <label htmlFor="#" className="form__title">Message</label>
-          </form>
+          <p className="form__text">Hello again, this form will help you to give me a brief introduction on how I can help you in your project. Should you have any questions, do not hesitate to write them down. it will <span>only take you less than 3 minutes.</span></p>
+          <FormContainer classForm='form__container'/>
       </section>
   )
 };
