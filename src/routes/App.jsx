6af74e0346eb6f4ef20fd2385f7e3f9c8b041e2c
Joan Gimenez <joanf.gimenez@gmail.com>
2022-01-28 16:12:38 -0300

    [UPDATE] ScrollToTop logic & change path contact

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -8,6 +8,7 @@ import Home         from '@pages/Home'
 import Error404     from '@pages/Error404'
 import ProjectsPage from '@pages/ProjectsPage'
 import Form         from '@pages/Form'
+import ScrollToTop  from '@services/ScrollToTop'
 import '@styles/app.scss'
 
 const App = () => {
@@ -17,11 +18,12 @@ const App = () => {
           basename='/'
           >
             <Layout>
+                <ScrollToTop/>
                 <Routes>
                   <Route path='/' element={ <Home/> } />
                   <Route path='/projects' element={ <ProjectsPage/> } />
                   <Route path='/projects/*' element={ <ProjectsPage/> } />
-                  <Route path='/form' element={ <Form /> } />
+                  <Route path='/contact' element={ <Form /> } />
                   <Route path='*' element={ <Error404/> } />
                 </Routes>
             </Layout>
