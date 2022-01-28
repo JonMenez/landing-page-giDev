c027797c76d88e76493f2f0bdddec30dcecd2e33
Joan Gimenez <joanf.gimenez@gmail.com>
2022-01-28 16:10:37 -0300

    [UPDATE] Button component rig CTAButton

--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -1,12 +1,18 @@
-import * as React from 'react';
-import CTAButton from '@components/CTAButton';
+import  React from 'react';
+import { useNavigate }   from 'react-router-dom'
+import Button from '@components/Button';
 import '@styles/contact.scss'
 
 const Contact = () => {
+
+  const navigate = useNavigate()
+  
+  const toContact = () => navigate('/contact')
+
   return (
          <section className='contact'>
              <h3 className='contact__title'>Let's talk about your project!</h3>
-             <CTAButton class='cta__button' />
+             <Button class='contact__button' name="Contact Me" click={toContact} />
          </section>
   )
 };
--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,14 +1,24 @@
 import * as React from 'react';
-import { Link }   from 'react-router-dom'
-import Slider     from '../components/Slider';
+import { useNavigate }   from 'react-router-dom'
+import Slider     from '@components/Slider';
+import Button     from '@components/Button';
 import '@styles/error404.scss'
 
 const Error404 = () => {
+
+  const navigate = useNavigate()
+
+  const backToHome = () => navigate('/')
+
   return (
       <section className="error404">
           <h1 className="error404__title">404</h1>
-          <p className="error404__text">We couldn’t find the page you were looking for, but don't worry!</p>
-          <button className="error404__button"><Link to='/' className="error404__backHome">Back to Home</Link></button>
+          <p className="error404__text">We couldn't find the page you were looking for, but don't worry!</p>
+          <Button 
+            class="error404__button" 
+            name='Back to Home'
+            click={backToHome} 
+            />
           <p className="error404__text">Or you can have a look at some of my projects</p>
           <Slider 
           idStyle='error404__slider'
