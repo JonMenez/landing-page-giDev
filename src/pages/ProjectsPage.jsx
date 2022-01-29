6970693f710bd3f9e676c03a7490bada7d5a412f
Joan Gimenez <joanf.gimenez@gmail.com>
2022-01-29 12:04:52 -0300

    [ADD] back button & elements

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,10 +1,19 @@
-import * as React from 'react';
-import Slider from '@components/Slider'
+import * as React  from 'react';
+import BackButton  from '@components/BackButton';
+import CardProject from '@components/CardProject'
+import '@styles/slider.scss'
+import '@styles/projectsPage.scss'
 
 const ProjectsPage = () => {
   return (
-      <section>
-          <h1>Projects</h1>
+      <section className='projectsPage'>
+        <BackButton/>
+          <h1 className='projectsPage__title'>Projects</h1>
+          <div className='projectsPage__container'>
+            <CardProject class={'projectsPage__card'}/>
+            <CardProject class={'projectsPage__card'}/>
+            <CardProject class={'projectsPage__card'}/>
+          </div>
       </section>
   );
 };
