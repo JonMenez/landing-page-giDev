c879d94671b8e955de37cc50e19a499dd4d3315f
Joan Gimenez <joanf.gimenez@gmail.com>
2022-01-29 12:03:32 -0300

    [ADD] props

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -2,9 +2,9 @@ import * as React from 'react'
 import projectImg from '@images/project.jpeg'
 import '@styles/cardProject.scss'
 
-const CardProject = () => {
+const CardProject = (props) => {
     return (
-        <div className='projects__card'>
+        <div className={`projects__card ${props.class}`}>
             <img className='projects__img' src={ projectImg } alt='' />
             <h3 className="projects__name">Project One</h3>
             <p className="projects__body">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
