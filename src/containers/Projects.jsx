3a9a4d8931ade38114c1d930f17fcb46fb289188
Joan Gimenez <joanf.gimenez@gmail.com>
2022-01-29 12:04:05 -0300

    [ADD] Link

--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { Link } from 'react-router-dom'
 import wave1 from '@icons/p1-wave.svg'
 import wave2 from '@icons/p2-wave.svg'
 import Slider from '../components/Slider'
@@ -10,7 +11,7 @@ const Projects = () => {
             <img className='projects__wave1' src={ wave1 } alt="svg wave" />
               <h2 className='projects__title'>My lastest Projects</h2>
               <Slider idStyle='projects__slider'/>
-              <a href='#' className="projects__more">See all</a>
+              <Link to='/projects' className="projects__more">See all</Link>
             <img className='projects__wave2' src={ wave2 } alt="svg wave" />
         </section>
     )
