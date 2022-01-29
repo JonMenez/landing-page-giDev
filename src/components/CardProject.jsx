2d388ed9356f5307d83482ead63a8890e723a89a
Joan Gimenez <joanf.gimenez@gmail.com>
2022-01-29 15:48:32 -0300

    [ADD] <Link/> react router

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -1,13 +1,16 @@
 import * as React from 'react'
+import { Link } from 'react-router-dom'
 import projectImg from '@images/project.jpeg'
 import '@styles/cardProject.scss'
 
 const CardProject = (props) => {
     return (
         <div className={`projects__card ${props.class}`}>
+            <Link to='Projects/*'>
             <img className='projects__img' src={ projectImg } alt='' />
             <h3 className="projects__name">Project One</h3>
             <p className="projects__body">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Link>
         </div>
     )
 }
