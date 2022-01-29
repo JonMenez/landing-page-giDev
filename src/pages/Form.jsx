952a2c85bbfe29fc4e803f8e1b20f7e42c168601
Joan Gimenez <joanf.gimenez@gmail.com>
2022-01-29 12:04:23 -0300

    [ADD] back button

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,10 +1,12 @@
-import * as React from 'react';
+import * as React    from 'react';
 import FormContainer from '@containers/FormContainer';
+import BackButton    from '@components/BackButton';
 import '@styles/form.scss'
 
 const Form = () => {
   return (
       <section className='form'>
+        <BackButton/>
           <h1 className="form__title">Contact</h1>
           <p className="form__text">Hello again, this form will help you to give me a brief introduction on how I can help you in your project. Should you have any questions, do not hesitate to write them down. it will <span>only take you less than 3 minutes.</span></p>
           <FormContainer classForm='form__container'/>
