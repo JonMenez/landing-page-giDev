de38787128c64d8632281b7e437c296635e472a7
Joan Gimenez <joanf.gimenez@gmail.com>
2022-01-31 12:03:09 -0300

    [ADD] individual className

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,10 @@ const Navbar = (props) => {
     return (
         <nav className='navBar'>
           <ul className={props.navBar} id={props.active}>
-              <li className={props.navList}><Link className={props.navLinks} to="/">Home</Link></li>
+              <li className={props.navList}><Link className={`${props.navLinks} navBar__home`} to="/">Home</Link></li>
               <li className={props.navList}><Link className={props.navLinks} to="#">Services</Link></li>
               <li className={props.navList}><Link className={props.navLinks} to="/projects">Projects</Link></li>
-              <li className={props.navList}><Link className={props.navLinks} to="/contact">Contact</Link></li>
+              <li className={props.navList}><Link className={`${props.navLinks} navBar__contactMe`} to="/contact">Contact Me</Link></li>
           </ul> 
         </nav>
     )
