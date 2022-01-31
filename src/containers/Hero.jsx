858ceaf567e39029d11d22e998957029dda47c2e
Joan Gimenez <joanf.gimenez@gmail.com>
2022-01-31 12:04:33 -0300

    [ADD] Contact button

--- src/containers/Hero.jsx
+++ src/containers/Hero.jsx
@@ -1,11 +1,19 @@
-import * as React from 'react'
+import * as React      from 'react'
+import { useNavigate } from 'react-router-dom'
+import Button from '@components/Button'
 import '@styles/hero.scss'
 
 const Hero = () => {
+
+  const navigate = useNavigate()
+  
+  const toContact = () => navigate('/contact')
+
     return (
         <section className='hero'>
             <h1 className='hero__title'>Hey there,<span>I'm Joan</span></h1>
             <p className='hero__body'>I'm a Front-end Web Developer who is gonna help you with your business</p>
+            <Button class='hero__button' name="Contact Me" click={toContact} />
         </section>
     )
 }
