156e9de61f37b8751a7211c1e3f728a32e5122cc
Joan Gimenez <joanf.gimenez@gmail.com>
2022-02-01 20:34:54 -0300

    [UPDATE] shapes SVG out of section

--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -2,18 +2,22 @@ import * as React from 'react'
 import { Link } from 'react-router-dom'
 import wave1 from '@icons/p1-wave.svg'
 import wave2 from '@icons/p2-wave.svg'
-import Slider from '../components/Slider'
+import Slider from '@components/Slider'
 import '@styles/projects.scss'
 
 const Projects = () => {
     return (
-        <section className='projects'>
+        <>
             <img className='projects__wave1' src={ wave1 } alt="svg wave" />
+        <section className='projects'>
+            <div className='projects__container'>
               <h2 className='projects__title'>My lastest Projects</h2>
               <Slider idStyle='projects__slider'/>
               <Link to='/projects' className="projects__more">See all</Link>
-            <img className='projects__wave2' src={ wave2 } alt="svg wave" />
+            </div>
         </section>
+            <img className='projects__wave2' src={ wave2 } alt="svg wave" />
+        </>
     )
 }
 
