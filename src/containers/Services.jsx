cc3d28cfb772c36de83cda8813ba4e0c22f5ea67
Joan Gimenez <joanf.gimenez@gmail.com>
2022-02-02 16:17:04 -0300

    [ADD] id

--- src/containers/Services.jsx
+++ src/containers/Services.jsx
@@ -5,7 +5,7 @@ import '@styles/services.scss'
 
 const Services = () => {
     return (
-        <section className='services'>
+        <section className='services' id='services'>
             <h2 className='services__title'>What I can to for you</h2>
             {
               services.map( service => <CardService key={service.id} services={service}/>)
