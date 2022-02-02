cfd2bb88a8ef02de09ee5f9d94ad6e507ce48ffe
Joan Gimenez <joanf.gimenez@gmail.com>
2022-02-02 16:18:13 -0300

    [ADD] props

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -3,15 +3,15 @@ import { useNavigate } from 'react-router-dom';
 import arrowBack from '@icons/arrow-back.svg'
 import '@styles/backButton.scss'
 
-const BackButton = () => {
+const BackButton = (props) => {
 
   const navigate = useNavigate()
 
   const goBack = () => navigate(-1)
 
   return (
-      <button className='backButton' onClick={ goBack }>
-          <img className='backButton__icon' src={ arrowBack } alt='arrow back icon' />
+      <button className={`backButton ${props.style}`} onClick={ goBack }>
+          <img className='backButton__icon' style={props.style} src={ arrowBack } alt='arrow back icon' />
       </button>
   )
 };
