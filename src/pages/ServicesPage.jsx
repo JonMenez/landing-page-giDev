b196f3985374baf3094fe491db25a3953cae7a5b
Joan Gimenez <joanf.gimenez@gmail.com>
2022-02-03 17:45:21 -0300

    [ADD] styles

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import * as  React from 'react';
+import '@styles/servicesPage.scss'
+
 
 const ServicesPage = () => {
   return (
       <div className="servicesPage">
           <h1 className="servicesPage__title">Services</h1>
+          Work in process
       </div>
   )
 };
