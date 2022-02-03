45269f287e46791b9d6177e0dd2c97a0ca6a52db
Joan Gimenez <joanf.gimenez@gmail.com>
2022-02-03 16:30:29 -0300

    [ADD] ServicesPage

---
+++ src/pages/ServicesPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ServicesPage = () => {
+  return (
+      <div className="servicesPage">
+          <h1 className="servicesPage__title">Services</h1>
+      </div>
+  )
+};
+
+export default ServicesPage;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,9 +5,10 @@ import {
     Route }         from 'react-router-dom'
 import Layout       from '@containers/Layout'
 import Home         from '@pages/Home'
-import Error404     from '@pages/Error404'
 import ProjectsPage from '@pages/ProjectsPage'
+import ServicesPage from '@pages/ServicesPage'
 import Form         from '@pages/Form'
+import Error404     from '@pages/Error404'
 import ProjectDetail from '../pages/ProjectDetail'
 import ScrollToTop  from '@services/ScrollToTop'
 import '@styles/app.scss'
@@ -24,6 +25,7 @@ const App = () => {
                   <Route path='/' element={ <Home/> } />
                   <Route path='/projects' element={ <ProjectsPage/> } />
                   <Route path='/projects/*' element={ <ProjectDetail/> } />
+                  <Route path='/services' element={ <ServicesPage /> } />
                   <Route path='/contact' element={ <Form /> } />
                   <Route path='*' element={ <Error404/> } />
                 </Routes>
