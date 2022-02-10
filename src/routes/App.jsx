6ed6137390c229fb3370c1958d91390018bf3431
Joan Gimenez <joanf.gimenez@gmail.com>
2022-02-10 09:45:59 -0300

    [UPDATE] component

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,35 +1,13 @@
-import * as React   from 'react'
-import {  
-    BrowserRouter, 
-    Routes, 
-    Route }         from 'react-router-dom'
-import Layout       from '@containers/Layout'
-import Home         from '@pages/Home'
-import ProjectsPage from '@pages/ProjectsPage'
-import ServicesPage from '@pages/ServicesPage'
-import Form         from '@pages/Form'
-import Error404     from '@pages/Error404'
-import ProjectDetail from '../pages/ProjectDetail'
-import ScrollToTop  from '@services/ScrollToTop'
+import * as React        from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import AnimatePage from './AnimatePage'
 import '@styles/app.scss'
 
 const App = () => {
     return (
         <div className='app'>
-          <BrowserRouter
-          basename='/'
-          >
-            <Layout>
-                <ScrollToTop/>
-                <Routes>
-                  <Route path='/' element={ <Home/> } />
-                  <Route path='/projects' element={ <ProjectsPage/> } />
-                  <Route path='/projects/*' element={ <ProjectDetail/> } />
-                  <Route path='/services' element={ <ServicesPage /> } />
-                  <Route path='/contact' element={ <Form /> } />
-                  <Route path='*' element={ <Error404/> } />
-                </Routes>
-            </Layout>
+          <BrowserRouter>
+               <AnimatePage />
           </BrowserRouter>
         </div>
     )
