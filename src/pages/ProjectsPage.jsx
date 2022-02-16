34ab22b429aed618adcef682623d7cfb110b8f6f
Joan Gimenez <joanf.gimenez@gmail.com>
2022-02-16 11:44:34 -0300

    [FIX] transition page

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -18,7 +18,7 @@ const pageVariant = {
   exit: {
     x: "100vw",
     opacity: 0,
-    transition: { duration: 0.5, ease: "easeInOut" },
+    transition: { duration: 1, ease: "easeInOut" },
   },
 };
 
