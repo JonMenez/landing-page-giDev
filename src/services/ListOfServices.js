abfdec3eb3cbcc29fd9ab782b444eabcb3f102d4
Joan Gimenez <joanf.gimenez@gmail.com>
2022-02-16 10:53:01 -0300

    [FIX] format

--- src/services/ListOfServices.js
+++ src/services/ListOfServices.js
@@ -1,33 +1,35 @@
-import webIcon       from '@icons/web-developer.svg'
-import desingIcon    from '@icons/web-design.svg'
-import ecommerceIcon from '@icons/ecommerce.svg'
-import brandingIcon  from '@icons/branding.svg'
+import webIcon from "@icons/web-developer.svg";
+import desingIcon from "@icons/web-design.svg";
+import ecommerceIcon from "@icons/ecommerce.svg";
+import brandingIcon from "@icons/branding.svg";
 
- export const services = [
-    {
+export const services = [
+  {
     id: 101,
-    title: 'Web Development',
+    title: "Web Development",
     icon: webIcon,
-    content: 'Coding is my passion and I can do it for you to help your business reach a new level!',
-   },
-    {
+    content:
+      "Coding is my passion and I can do it for you to help your business reach a new level!",
+  },
+  {
     id: 102,
-    title: 'Web Design',
+    title: "Web Design",
     icon: desingIcon,
-    content: 'A well design site will help you catch your customers atention and increase your sales',
-   },
-    {
+    content:
+      "A well design site will help you catch your customers atention and increase your sales",
+  },
+  {
     id: 103,
-    title: 'e-commerce',
+    title: "e-commerce",
     icon: ecommerceIcon,
-    content: 'e commerce is future and present and I can be your partner in business building your store!',
-   },
-    {
+    content:
+      "e commerce is future and present and I can be your partner in business building your store!",
+  },
+  {
     id: 104,
-    title: 'Branding',
+    title: "Branding",
     icon: brandingIcon,
-    content: 'A strong brand will stay in your clients mind and I know the tactics to make them fall in love with you ',
-   }
-
-]
-
+    content:
+      "A strong brand will stay in your clients mind and I know the tactics to make them fall in love with you ",
+  },
+];
