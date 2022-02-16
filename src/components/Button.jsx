64628de215dbfa8e8f15cb5e68655f3439a548b1
Joan Gimenez <joanf.gimenez@gmail.com>
2022-02-16 12:23:09 -0300

    [ADD] framer motion

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,32 @@
-import * as React from 'react';
+import * as React from "react";
+import { motion } from "framer-motion";
+
+const buttonVariant = {
+  hidden: {
+    x: "-100vw",
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { delay: 1.5, type: "spring", duration: 0.5 },
+  },
+  exit: {
+    scale: 0,
+  },
+};
 
 const Button = (props) => {
   return (
-       <button className={ props.class } type={props.type} onClick={props.click}>
-           {props.name}
-       </button>
-  )
+    <motion.button
+      className={props.class}
+      variants={buttonVariant}
+      type={props.type}
+      onClick={props.click}
+    >
+      {props.name}
+    </motion.button>
+  );
 };
 
 export default Button;
