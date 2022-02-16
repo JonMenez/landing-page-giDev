198d0a6efbc26248d95b85191a3f116c4f676dd5
Joan Gimenez <joanf.gimenez@gmail.com>
2022-02-16 10:52:20 -0300

    [ADD] hooks alias

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,66 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-		publicPath: '/',
-		assetModuleFilename: 'images/[hash][ext][query]'
-	},
-	mode: 'development',
-	resolve: {
-		extensions: ['.js', '.jsx'],
-		alias: {
-			'@components': path.resolve(__dirname, 'src/components/'),
-			'@containers': path.resolve(__dirname, 'src/containers/'),
-			'@styles'    : path.resolve(__dirname, 'src/styles/'),
-			'@icons'     : path.resolve(__dirname, 'src/assets/icons/'),
-			'@images'    : path.resolve(__dirname, 'src/assets/images/'),
-			'@pages'     : path.resolve(__dirname, 'src/pages/'),
-			'@services'  : path.resolve(__dirname, 'src/services/'),
-		}
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader'
-				}
-			},
-			{
-				test: /\.html$/,
-				use: [
-					{
-						loader: 'html-loader'
-					}
-				]
-			},
-			{
-				test: /\.(css|scss)$/,
-				use: [
-					"style-loader",
-					"css-loader",
-					"sass-loader",
-				],
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/,
-				type: 'asset'
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './public/index.html',
-			filename: './index.html'
-		}),
-		new MiniCssExtractPlugin({
-			filename: '[name].css'
-		}),
-	],
-	devServer: {
-		historyApiFallback: true,
-	}
-}
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "@components": path.resolve(__dirname, "src/components/"),
+      "@containers": path.resolve(__dirname, "src/containers/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+      "@icons": path.resolve(__dirname, "src/assets/icons/"),
+      "@images": path.resolve(__dirname, "src/assets/images/"),
+      "@pages": path.resolve(__dirname, "src/pages/"),
+      "@services": path.resolve(__dirname, "src/services/"),
+      "@hooks": path.resolve(__dirname, "src/hooks/"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: "asset",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+};
